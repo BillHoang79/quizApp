cb4e85844922d664a49564f77326b33f0c14c475
bill hoang <billhoang79@gmail.com>
2016-11-15 20:27:48 -0800

    simple quiz app

--- js/main.js
+++ js/main.js
@@ -1,22 +1,57 @@
-function addItem(){
-	//user types in item to add on list
-	var userInput = document.getElementById('input').value
-	//the item is them taken and stored in text
-	var text = document.createTextNode(userInput)
-	//text becomes li and is ready to be put on list
-	var li = document.createElement('li')
-	//item list is retrieved and li is taken 
-	document.getElementById('items').appendChild(li)
-	//li is added to the list with this 
-	li.appendChild(text)
-
-function removeItem() {
-    li.className = "remove"
+//array of questions and answers
+var quizArray = [
+	{
+		question: "What color is the Sun?",
+		answer: "orange"
+	},
+	{
+		question: "how many Moons does the Earth have?",
+		answer: 1
+	},
+	{
+		question: "What is 12 + 11?",
+		answer: 33
+	},
+	{
+		question: "How many inches in a foot?",
+		answer: 12
+	},
+	{
+		question: "Biggest search engine in the world right now?",
+		answer: "google"
+	},
+	{
+		question: "The current season The Simpsons' is on?",
+		answer: "28"
+	},
+	{
+		question: "Java------ is cool!!",
+		answer: "script"
 	}
-	li.onclick = removeItem
+]
+
+for(i = 0; i < quizArray.length; i++) {
+ var q = quizArray[i].question
+	document.getElementById('question' +[i]).textContent = q
 }
 
+function getInfo() {
+	var correct = 0
+	var incorrect = 0
 
-
-
+	for(i = 0; i < quizArray.length; i++) {
+		var solution = quizArray[i].answer
+		var answer = document.getElementById('answer' + [i]).value
+		var askQuestion = document.getElementById('question' + [i])
+		if(solution == answer) {
+			askQuestion.className = 'correct'
+			correct++
+		} else {
+			askQuestion.className = 'incorrect'
+			incorrect++
+		}
+	}
+	document.getElementById('correct').textContent = correct
+	document.getElementById('incorrect').textContent= incorrect
+}
 
